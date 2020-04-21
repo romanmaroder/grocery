f284d8812cbf780c30ee0427a6f4a58aaab79572
Roman <kashik.roman@rambler.ru>
2020-04-21 19:29:43 +0300

    Install Yii2 URL setting Layout

---
+++ web/js/main.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function ($) {
+
+    // start-smoth-scrolling
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+    });
+
+    //script-for sticky-nav
+    var navoffeset = $(".agileits_header").offset().top;
+    $(window).scroll(function () {
+        var scrollpos = $(window).scrollTop();
+        if (scrollpos >= navoffeset) {
+            $(".agileits_header").addClass("fixed");
+        } else {
+            $(".agileits_header").removeClass("fixed");
+        }
+    });
+
+    // Bootstrap Core JavaScript
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+
+    // here stars scrolling icon
+
+    /*
+           var defaults = {
+           containerID: 'toTop', // fading element id
+           containerHoverID: 'toTopHover', // fading element hover id
+           scrollSpeed: 1200,
+           easingType: 'linear'
+           };
+       */
+    $().UItoTop({easingType: 'easeOutQuart'});
+
+});
+
+// flexSlider
+$(window).load(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        start: function (slider) {
+            $('body').removeClass('loading');
+        }
+    });
+});
+
+paypal.minicart.render();
+
+paypal.minicart.cart.on('checkout', function (evt) {
+    var items = this.items(),
+        len = items.length,
+        total = 0,
+        i;
+
+    // Count the number of each item in the cart
+    for (i = 0; i < len; i++) {
+        total += items[i].get('quantity');
+    }
+
+    if (total < 3) {
+        alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+        evt.preventDefault();
+    }
+});
